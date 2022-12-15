5d01a77d91c7f0377f1b416204910df5a4deb7c9
Ofili Ngozichukwuka Tamaralayefa <fredili482@gmail.com>
2022-12-15 12:53:23 -0800

    dd

--- stacks.c
+++ stacks.c
@@ -1,10 +1,8 @@
-nclude "monty.h"
+#include "monty.h"
 #include <string.h>
-
 void free_stack(stack_t **stack);
 int init_stack(stack_t **stack);
 int check_mode(stack_t *stack);
-
 /**
  * free_stack - Frees a stack_t stack.
  * @stack: A pointer to the top (stack) or
@@ -12,16 +10,14 @@ int check_mode(stack_t *stack);
  */
 void free_stack(stack_t **stack)
 {
-	stack_t *tmp = *stack;
-
-	while (*stack)
-	{
-		tmp = (*stack)->next;
-		free(*stack);
-		*stack = tmp;
-	}
+stack_t *tmp = *stack;
+while (*stack)
+{
+tmp = (*stack)->next;
+free(*stack);
+*stack = tmp;
 }
-
+}
 /**
  * init_stack - Initializes a stack_t stack with beginning
  *              stack and ending queue nodes.
@@ -32,21 +28,16 @@ void free_stack(stack_t **stack)
  */
 int init_stack(stack_t **stack)
 {
-	stack_t *s;
-
-	s = malloc(sizeof(stack_t));
-	if (s == NULL)
-		return (malloc_error());
-
-	s->n = STACK;
-	s->prev = NULL;
-	s->next = NULL;
-
-	*stack = s;
-
-	return (EXIT_SUCCESS);
+stack_t *s;
+s = malloc(sizeof(stack_t));
+if (s == NULL)
+return (malloc_error());
+s->n = STACK;
+s->prev = NULL;
+s->next = NULL;
+*stack = s;
+return (EXIT_SUCCESS);
 }
-
 /**
  * check_mode - Checks if a stack_t linked list is in stack or queue mode.
  * @stack: A pointer to the top (stack) or bottom (queue)
@@ -58,9 +49,9 @@ int init_stack(stack_t **stack)
  */
 int check_mode(stack_t *stack)
 {
-	if (stack->n == STACK)
-		return (STACK);
-	else if (stack->n == QUEUE)
-		return (QUEUE);
-	return (2);
+if (stack->n == STACK)
+return (STACK);
+else if (stack->n == QUEUE)
+return (QUEUE);
+return (2);
 }
